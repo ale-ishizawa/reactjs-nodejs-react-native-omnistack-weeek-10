694bdf8f694115ff87c3b538ab886416a143e31c
Alessandro Navarro Ishizawa <a.navarro.ishizawa@gmail.com>
2020-01-17 17:41:14 -0300

    interface web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://alessandro:semana@cluster0-4hdqu.mongodb.net/wee
 })
 
 //Entende requisições que tem corpo no formato JSON
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
